eda29119725751f90a28c0c664b01381bc2ab9a5
Harishkumar79 <harishsen7976@gmail.com>
2026-04-29 22:44:15 +0530

    Fix merge conflicts

--- src/views/Info.jsx
+++ src/views/Info.jsx
@@ -4,19 +4,8 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import "../../public/css/info.css";
 
-export default function Info() {
+export default function Info({info}) {
     let demoImg = "https://images.unsplash.com/photo-1561553873-e8491a564fd0?q=80&w=1247&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-
-
-    let info = {
-        city: "sirohi",
-        temp: 30,
-        tempmin: 29,
-        tempmax: 32,
-        humidity: 32,
-        feelslike: 28.8,
-        weather: "clear sky"
-    }
 
     return (
         <div className='infoContainer'>
@@ -35,7 +24,7 @@ export default function Info() {
                         <p>Max Temp : {info.tempmax}&deg;C</p>
                         <p>Min Temp : {info.tempmin}&deg;C</p>
                         <p>Humidity : {info.humidity}</p>
-                        <p>Current {info.city} weather is {info.weather} and feels like {info.feelslike}</p>
+                        <p>The weather can be described as <b><i>{info.weather}</i></b> and feels like {info.feelslike}&deg;C</p>
                     </Typography>
                 </CardContent>
             </Card>
--- src/views/Weather.jsx
+++ src/views/Weather.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import Info from "./Info";
 import Search from "./Search";
 
 export default function Weather() {
+
+    let [weatherInfo , setWeatherInfo] = useState({
+        city: "sirohi",
+        temp: 30,
+        tempmin: 29,
+        tempmax: 32,
+        humidity: 32,
+        feelslike: 28.8,
+        weather: "clear sky"
+    })
+
     return (
         <div>
-            <Search />
+            <Search info={weatherInfo}/>
             <br/>
-            <Info />
+            <Info info={weatherInfo} />
         </div>
     )
 }
